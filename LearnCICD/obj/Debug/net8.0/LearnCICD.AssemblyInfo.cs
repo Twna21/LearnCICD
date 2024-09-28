ae305d215cddf0abd667570cd8b1ab790fa310b3
twna21 <132243099+Twna21@users.noreply.github.com>
2024-09-28 15:48:02 +0700

    merge develop v1

--- LearnCICD/obj/Debug/net8.0/LearnCICD.AssemblyInfo.cs
+++ LearnCICD/obj/Debug/net8.0/LearnCICD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnCICD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e24ba9248c81bf8213d5b49183b10b0c431c7bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnCICD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnCICD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
